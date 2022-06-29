3942e4858e4775391b9bea1927906a2c5e9ec574
Sercan Tuna <ssercantuna@hotmail.com>
2022-06-29 17:45:00 -0400

    updated

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -11,19 +11,23 @@ public class Runner {
                 System.out.println("1. Check-in");
                 System.out.println("2. Check-out");
                 String customerSelection = scanner.nextLine();
+                int selectionNum = Integer.parseInt(customerSelection);
 
-                if (customerSelection.equals("1")) {
-                    System.out.println("What is your name?");
-                    String customerName = scanner.nextLine();
+                if (selectionNum < 1 || selectionNum > 2) {
+                    throw new InputMismatchException("Option must be either 1 or 2");
+
+                }
+
+                System.out.println("What is your name?");
+                String customerName = scanner.nextLine();
+
+                if (selectionNum == 1) {
+
                     restaurant.checkIn(customerName.toLowerCase());
-                } else if (customerSelection.equals("2")) {
+                } else if (selectionNum == 2) {
 
-                    System.out.println("What is your name?");
-                    String customerName = scanner.nextLine();
                     restaurant.checkOut(customerName.toLowerCase());
 
-                } else {
-                    throw new InputMismatchException("Option must be either 1 or 2");
                 }
 
             }
